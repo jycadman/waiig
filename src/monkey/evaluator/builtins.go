ed51018167220e22043be385979e126c00b52218
jycadman <jycadman@unm.edu>
2024-06-20 02:51:24 -0600

    Added  built-in function to print given arguments

--- src/monkey/evaluator/builtins.go
+++ src/monkey/evaluator/builtins.go
@@ -3,6 +3,7 @@
 package evaluator
 
 import (
+	"fmt"
 	"monkey/object"
 )
 
@@ -23,6 +24,15 @@ var builtins = map[string]*object.Builtin{
 				return newError("argument to `len` not supported, got %s",
 					args[0].Type())
 			}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 	"first": &object.Builtin{
